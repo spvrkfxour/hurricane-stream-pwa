8469f3df98d636df2ce5b82986e907b0623881fb
spvrkfxour <spvrkfxour@gmail.com>
2023-03-11 23:34:54 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => console.log("Service worker registered"))
+      .catch((error) => console.error("Error registering service worker"));
+  }
+  
 ReactDOM.render(<App />, document.getElementById('root'));
